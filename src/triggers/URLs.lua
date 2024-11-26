90346569f2c676e2ee46c0ce7dbcb9f8ac1fff9a
gesslar <karahd@gmail.com>
2024-11-26 17:42:06 -0500

    updating iconography and fixing the fact that things that aren't URLs were highlighting

--- src/triggers/URLs.lua
+++ src/triggers/URLs.lua
@@ -1,6 +1,12 @@
 local url = matches[1]
+
+-- Highlight the URL
 selectString(url, 1)
-setLink(function() openUrl(url) end, f"Open {url} in your browser!")
+setLink(function() openUrl(url) end, f"Open {url} in your browser.")
 setBold(true)
 setUnderline(true)
+
+-- Now undo the changes
 deselect()
+setBold(false)
+setUnderline(false)
